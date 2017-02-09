32a54d512fe6367a8aa250b862a2dd491afb59a1
Flarpz <Marcuskarrm@gmail.com>
2017-02-09 13:54:02 +0100

    fix for db

--- stream_socket/server.js
+++ stream_socket/server.js
@@ -4,13 +4,13 @@ const ioClient = require('socket.io-client');
 const fs = require('fs');
 
 
-var socket = ioClient.connect("http://35.157.38.255/");
+var socket = ioClient.connect("http://localhost:3000");
 
 let accX = fs.readFileSync('accX.txt').toString().split('\n').map(Number);
 let accY = fs.readFileSync('accY.txt').toString().split('\n').map(Number);
 let accZ = fs.readFileSync('accZ.txt').toString().split('\n').map(Number);
 
-socket.emit('register', {type:'publisher', freq:'128.00'});
+socket.emit('register', {user:'Marcus', type:'publisher', freq:'128.00'});
 
 socket.on('registered', (data)=>{
   console.log(data.id);
@@ -21,13 +21,13 @@ function sendAllData(x,y,z){
   console.log('sending all the data..')
   var i = 0;
   let loop =setInterval(function(){
-    socket.emit('accelormeter_input', {x: x[i], y:y[i], z:z[i]});
+    socket.emit('accelormeter_input', {x: x[i], y:y[i], z:z[i], client_ts: Date.now(), index: i});
     i++;
     if(i === 6000){
       clearInterval(loop);
     }
   },8);
 
-  console.log('fäääärdig');
+  console.log('Done');
 
 }
