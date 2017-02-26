728b7e46f1d40c0724aa9e5c5b4763a16aaea1dc
Joao Bentes <joaobentes.junior@gmail.com>
2017-02-26 17:33:48 +0100

    Added two sensors at time

--- stream_socket/server.js
+++ stream_socket/server.js
@@ -6,32 +6,36 @@ const fs = require('fs');
 
 var socket = ioClient.connect("http://localhost:3000/");
 
-let accX = fs.readFileSync('accX.txt').toString().split('\n').map(Number);
-let accY = fs.readFileSync('accY.txt').toString().split('\n').map(Number);
-let accZ = fs.readFileSync('accZ.txt').toString().split('\n').map(Number);
+let user1_accX = fs.readFileSync('User1_accX.txt').toString().split('\n').map(Number);
+let user1_accY = fs.readFileSync('User1_accY.txt').toString().split('\n').map(Number);
+let user1_accZ = fs.readFileSync('User1_accZ.txt').toString().split('\n').map(Number);
 
-socket.emit('register', {user:'Marcus', type:'publisher', freq:'128.00'});
+let user2_accX = fs.readFileSync('User2_accX.txt').toString().split('\n').map(Number);
+let user2_accY = fs.readFileSync('User2_accY.txt').toString().split('\n').map(Number);
+let user2_accZ = fs.readFileSync('User2_accZ.txt').toString().split('\n').map(Number);
+
+
+socket.emit('register', {user:'Marcus', type:'publisher', freq:'128.00', channel:0});
+socket.emit('register', {user:'Marcus', type:'publisher', freq:'128.00', channel:1});
 
 
 socket.on('registered', (data)=>{
   console.log(data.id);
   setTimeout(function(){
-      sendAllData(accX,accY,accZ);
-  },10000)
-
+      sendAllData(user1_accX,user1_accY,user1_accZ,0);
+      sendAllData(user2_accX,user2_accY,user2_accZ,1);
+  },10000);
 });
 
-function sendAllData(x,y,z){
+function sendAllData(x,y,z,channel){
   console.log('sending all the data..')
   var i = 0;
   let loop =setInterval(function(){
-    socket.emit('accelormeter_input', {x: x[i], y:y[i], z:z[i], client_ts: Date.now(), index: i});
+    socket.emit('accelormeter_input', {x: x[i], y:y[i], z:z[i], client_ts: Date.now(), index: i, channel:channel});
     i++;
-    if(i === 6500){
+    if(i === 30000){
       clearInterval(loop);
     }
   },8);
-
   console.log('Done');
-
 }
