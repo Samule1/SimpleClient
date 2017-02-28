26b585fad587a3d6083138197839d5006672de51
Samule1 <ham_car@hotmail.com>
2017-02-28 12:01:28 +0100

    for-joao

--- stream_socket/server.js
+++ stream_socket/server.js
@@ -4,7 +4,7 @@ const ioClient = require('socket.io-client');
 const fs = require('fs');
 
 
-var socket = ioClient.connect("http://localhost:3000/");
+var socket = ioClient.connect("http://localhost:3000");
 
 var rf = {
   x: fs.readFileSync('rf_accX.txt').toString().split('\n').map(Number),
@@ -20,24 +20,36 @@ var lf = {
 
 socket.emit('register', {user:'Marcus', type:'publisher', freq:'128.00', channel:0});
 
+
 setTimeout(function(){
-  socket.emit('register', {user:'Marcus', type:'publisher', freq:'128.00', channel:1});
+  socket.emit('register', {user:'Hampus', type:'publisher', freq:'128.00', channel:1});
 }, 1000);
+
+
+
+
+
+
 
 
 socket.on('registered', (data)=>{
   console.log(data.id);
-  setTimeout(function(){
-      sendAllData(rf, lf);
-  },10000);
+  if(data.channel === 0){
+      sendAllData(rf, 0);
+  }
+  if(data.channel === 1){
+      sendAllData(lf, 1);
+  }
+
+
 });
 
-function sendAllData(rf, lf){
+function sendAllData(points, channel){
   console.log('sending all the data..')
   var i = 0;
   let loop =setInterval(function(){
-    socket.emit('accelormeter_input', {x: rf.x[i], y:rf.y[i], z:rf.z[i], client_ts: Date.now(), index: i, channel:0});
-    socket.emit('accelormeter_input', {x: lf.x[i], y:lf.y[i], z:lf.z[i], client_ts: Date.now(), index: i, channel:1});
+    socket.emit('accelormeter_input', {x: points.x[i], y:points.y[i], z:points.z[i], client_ts: Date.now(), index: i, channel:channel});
+
     i++;
     if(i === 30000){
       clearInterval(loop);
